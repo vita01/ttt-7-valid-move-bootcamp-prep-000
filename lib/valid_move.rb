6ba2006ee9ca743b99eeaa1d038881260e83a40d
Victoria <vitalina27@gmail.com>
2017-12-11 11:25:34 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 
 def valid_move?(board, position)
 
-  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+return  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 
 
 end
