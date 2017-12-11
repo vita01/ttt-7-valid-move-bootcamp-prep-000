77447fd0f8bdd6f4190df0d69c8ce546f632284e
Victoria <vitalina27@gmail.com>
2017-12-11 11:31:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 
-def valid_move?(board, position)
+def valid_move?(board, position-1)
 
-if  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+if  position.to_i.between?(0,8) && !position_taken?(board, position.to_i)
   return true
 else return false
 end
