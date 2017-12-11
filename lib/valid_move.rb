560741314cd306060f53717208e56fe844f047cd
Victoria <vitalina27@gmail.com>
2017-12-11 12:34:41 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,17 +1,13 @@
-def valid_move?(board,position)
-  if  position.to_i.between?(1,9) && position_taken?(board,position.to_i-1)
-  return  true
-  else
-    return false
-  end
+def valid_move?(board, position)
+  # if position.to_i.between?(1,9)
+  #   if !position_taken?(board, position.to_i-1)
+  #     true
+  #   end
+  # end
+
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
 
-
-
-def position_taken?(board, index)
-   if board[index]=='' || board[index]==' ' || board[index]==nil
-      return false
-    else
-      return true
-    end
+def position_taken?(board, location)
+  board[location] != " "
 end
