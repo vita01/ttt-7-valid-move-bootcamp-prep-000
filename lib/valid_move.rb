5ac2a80363b509bebdc46ed0bf2a6abc71f0e2bc
Victoria <vitalina27@gmail.com>
2017-12-11 11:53:54 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,8 +12,8 @@ end
 def position_taken?(board, position)
 
   if ((board[position] == "X") || (board[position ] == "O"))
-      return true
-    else
       return false
+    else
+      return true
     end
   end
